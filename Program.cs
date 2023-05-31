81aa15223f30c6a42eda7b0736a0f5bd750014d0
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-31 13:08:29 +0530

    Add: maximum number at 1st position code is added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            Console.WriteLine(testClass.IntCompare(4, 3, 2));
+
+        }
+    }
+}
