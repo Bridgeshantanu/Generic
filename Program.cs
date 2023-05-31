a3e50ccba48dd460fb304ca1b9f1a0259a6a7f24
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-31 13:20:27 +0530

    Add: floats maximum numbres code is added

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@
             Console.WriteLine(testClass.IntCompare(4, 3, 2));
             Console.WriteLine(testClass.IntCompare(3, 4, 2));
             Console.WriteLine(testClass.IntCompare(2, 3, 4));
+            Console.WriteLine("----------------------------");
+
+            TestClass testClass1 = new TestClass();
+            Console.WriteLine(testClass1.floatCompare(4.1f, 3.2f, 2.3f));
+            Console.WriteLine(testClass1.floatCompare(3.2f, 4.1f, 2.3f));
+            Console.WriteLine(testClass1.floatCompare(3.2f, 2.3f, 4.1f));
 
         }
     }
